c48a47f585667f4ca91056c7dac07f6ee281e2f5
Dong <0x317374@gmail.com>
2021-11-29 11:15:39 +0700

    chore: drunkenly

--- packages/crack-services/app.ts
+++ packages/crack-services/app.ts
@@ -1,11 +1,25 @@
+import path = require("path");
+
 require("./gen-cert");
+
+import { readFileSync } from "fs";
 import express = require("express");
+import https = require("https");
 
 async function app() {
-  const app = express();
+  const credentials = {
+    key: readFileSync(path.join(__dirname, ".cert/key.pem"), "utf8"),
+    cert: readFileSync(path.join(__dirname, ".cert/cert.pem"), "utf8"),
+  };
   const port = process.env.PORT || 3000;
+  const app = express();
+  const httpsServer = https.createServer(credentials, app);
 
-  app.listen(port, () => {
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+
+  httpsServer.listen(port, () => {
     console.log(`App listening at https://localhost:${port}`);
   });
 }
